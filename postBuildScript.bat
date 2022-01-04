87bb85aba3e1d4437485d68520045206335ca46f
ckf42 <31157514+ckf42@users.noreply.github.com>
2022-01-04 14:26:09 +0800

    add postBuildScript to replace printerSettings1.bin

---
+++ postBuildScript.bat
@@ -0,0 +1,3 @@
+7z d build\skeleton.xlsm -tzip xl\printerSettings\printerSettings1.bin
+7z a build\skeleton.xlsm -tzip skeleton\xl\printerSettings\printerSettings1.bin
+7z rn build\skeleton.xlsm -ux1y1z1w1 -tzip skeleton\xl\printerSettings\printerSettings1.bin xl\printerSettings\printerSettings1.bin
