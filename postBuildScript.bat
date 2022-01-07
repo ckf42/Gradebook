f3301d1f320a1948cb6ff1900b7ecd8e8a9d5005
ckf42 <31157514+ckf42@users.noreply.github.com>
2022-01-07 15:25:07 +0800

    avoid checking in printerSettings, optimize postBuildScript

--- postBuildScript.bat
+++ postBuildScript.bat
@@ -1,3 +1,4 @@
-7z d build\skeleton.xlsm -tzip xl\printerSettings\printerSettings1.bin
-7z a build\skeleton.xlsm -tzip skeleton\xl\printerSettings\printerSettings1.bin
-7z rn build\skeleton.xlsm -ux1y1z1w1 -tzip skeleton\xl\printerSettings\printerSettings1.bin xl\printerSettings\printerSettings1.bin
+setlocal
+cd skeleton
+7z d ..\build\skeleton.xlsm -tzip xl\printerSettings\printerSettings1.bin
+7z a ..\build\skeleton.xlsm -tzip xl\printerSettings\printerSettings1.bin
